6aed94e94784c14f1e8af31b581b4f2e134a41e8
Dylan Powers <dylan.kyle.powers@gmail.com>
2015-11-05 23:55:38 -0800

    Request photos for album

--- src/main.go
+++ src/main.go
@@ -10,6 +10,10 @@ import (
 	"strings"
 )
 
+const facebookApiEndpoint = "https://graph.facebook.com/v2.5"
+const album2ID = "615849938471305"
+const album3ID = "855587011164262"
+
 func main() {
 	file, err := os.Open("fb-client-secret")
 	if err != nil {
@@ -19,12 +23,13 @@ func main() {
 
 	defer file.Close()
 	secret, err := ioutil.ReadAll(file)
-	fmt.Println(requestFBAccessToken(strings.TrimSpace(string(secret))))
+	accessToken := requestFBAccessToken(strings.TrimSpace(string(secret)))
+	println(string(requestPhotosForAlbum(accessToken, album2ID)))
 
-	http.HandleFunc("/", doShit)
-	if http.ListenAndServe(":8000", nil) != nil {
-		fmt.Println("We aren't doing shit")
-	}
+//	http.HandleFunc("/", doShit)
+//	if http.ListenAndServe(":8000", nil) != nil {
+//		fmt.Println("We aren't doing shit")
+//	}
 }
 
 func doShit(w http.ResponseWriter, r *http.Request) {
@@ -32,10 +37,8 @@ func doShit(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Did some shit\n")
 }
 
-
-
 func requestFBAccessToken(secret string) string {
-	resp, err := http.Get("https://graph.facebook.com/v2.5/oauth/access_token?" +
+	resp, err := http.Get(facebookApiEndpoint + "/oauth/access_token?" +
 					  "client_id=162401547444127" +
 					  "&client_secret=" + secret +
 					  "&grant_type=client_credentials")
@@ -59,4 +62,21 @@ func requestFBAccessToken(secret string) string {
 	}
 	m := f.(map[string]interface{})
 	return m["access_token"].(string)
+}
+
+func requestPhotosForAlbum(accessToken string, albumID string) []interface{} {
+	resp, err := http.Get(facebookApiEndpoint + "/" + album2ID + "/photos?" +
+						  "access_token=" + accessToken +
+						  "&fields=images")
+	if err != nil {
+		println("Whoops! WTF happened?")
+		return []interface{}{}
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	var f interface{}
+	err = json.Unmarshal(body, &f)
+	m := f.(map[string]interface{})
+	return m["data"].([]interface{})
 }
